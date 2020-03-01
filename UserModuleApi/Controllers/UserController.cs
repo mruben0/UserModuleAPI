e528fcee52d4727a862ce01d03f88bc2e04eb0e7
Ruben Martirosyan <mruben93@gmail.com>
2020-03-01 21:39:13 +0400

    initial commit

---
+++ UserModuleApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserModuleApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("yo")]     
+        public string GetUser()
+        {
+            return "user";
+        }
+    }
+}
